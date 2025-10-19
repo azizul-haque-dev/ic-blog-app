4f0c1c0efcf99289885e3c50dbcebd7d420b60fc
Lokman Sarkar <lokmansarker13@gmail.com>
2025-10-19 13:09:37 +0600

    User Profile routes and  Controller added

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -133,4 +133,38 @@ const uploadAvatar = async (req, res) => {
   }
 };
 
+
+
+export const getUserProfile = async (req, res) => {
+  try {
+
+    const userId = req.user.id;
+
+
+    const user = await UserModel.findById(userId).select(
+      "-password -emailVerificationToken -verificationTokenExpireAt -resetPasswordToken -resetPasswordExpires"
+    );
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found." });
+    }
+
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error("Get User Profile Error:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Server error. Please try again later." });
+  }
+};
+
+
+
+
 export { forgetPassword, resetPassword, uploadAvatar };
