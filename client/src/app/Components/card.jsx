8f0eacf685ca30f52beca5566df0fe40e7a3868d
Nasim <mnasimhosen11@gmail.com>
2025-10-21 01:26:46 +0600

    card component updated

--- client/src/app/Components/card.jsx
+++ client/src/app/Components/card.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Card({ post }) {
   const { title, content, imageUrl, categories, status, createdAt } = post;
 
@@ -18,7 +20,7 @@ export default function Card({ post }) {
         <div className="flex flex-wrap gap-2 mb-3">
           {categories.map((cat, index) => (
             <span
-              key={index}
+              key={cat}
               className="bg-blue-100 text-blue-700 px-2 py-1 rounded text-sm"
             >
               {cat}
@@ -36,9 +38,11 @@ export default function Card({ post }) {
       </div>
       {/* <div className="absolute bottom-0 left-0 w-full h-8 bg-gradient-to-t from-gray-100 to-transparent rounded-b-2xl"></div> */}
       <div className="flex justify-end mx-4 my-4">
-        <button className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white text-sm font-medium px-4 py-2 rounded-lg shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out cursor-pointer">
-          Read More
-        </button>
+        <Link href={`/post/${post.id}`}>
+          <button className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white text-sm font-medium px-4 py-2 rounded-lg shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out cursor-pointer">
+            Read More →
+          </button>
+        </Link>
       </div>
     </div>
   );
