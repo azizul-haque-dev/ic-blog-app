0709ea582988aa26df18b20ccfd144c8922fde8d
azizul-haque-dev <azizul.contact.me@gmail.com>
2025-10-21 22:37:36 +0300

    fixes tailwind issues

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
-  },
+    themes: ["light"]
+  }
 };
