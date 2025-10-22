cfc3e46f2c0e5fd34d1eb4587c041801d849f569
md sinikdho <mdsinikdho94@gmail.com>
2025-10-23 01:22:51 +0600

    update gitignore to exclude .next folder

--- client/.next/static/chunks/src_app_user_addPost_page_jsx_4ae868a2._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_user_addPost_page_jsx_21a24af7._.js"
-],
-    source: "dynamic"
-});
